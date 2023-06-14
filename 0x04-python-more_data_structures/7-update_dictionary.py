142ce9f21ce5e8c6d689b1de3199101872c29561
myname <myemail@gmail.com>
2023-06-14 10:43:09 -0700

    7-update_dictionary.py

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, price):
+    """Replace or add key/value pairs in a dictionary."""
+    a_dictionary[key] = price
+    return (a_dictionary)
