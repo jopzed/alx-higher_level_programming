6888cb8a9fe2f4d247910fffd230d26588c7822b
myname <myemail@gmail.com>
2023-07-10 12:38:38 -0700

    0x0A-python-inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines an object attribute lookup function."""
+
+
+def lookup(obj):
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
+#!/usr/bin/python3
+"""Defines an object attribute lookup function."""
+
+
+def lookup(obj):
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
